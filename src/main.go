16fe93beb67932c8af56b116ada04ffde2c2a954
docooler <laiyuan.yang@gmail.com>
2014-07-04 00:21:58 +0800

    [docooler] response by id

--- src/main.go
+++ src/main.go
@@ -1,51 +1,91 @@
-package main 
+package main
 
 import (
-         "net/http"
-         "log" 
-         "html/template"
-         "fmt"
-       )
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
 
-func rootHandler(w http.ResponseWriter, r * http.Request) {
-    r.ParseForm()
-if r.Method == "GET" {
-      t, err := template.ParseFiles("static/index.html")
-      if err != nil {
-         http.Error(w, err.Error(), http.StatusInternalServerError)
-         return
-      }
+const ADV_LEN = 5
 
-      t.Execute(w, nil )
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("../static/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-      
-      fmt.Println ("End root")
-      return
-   }
+		t.Execute(w, nil)
+
+		fmt.Println("End root")
+		return
+	}
 }
 
-func displayHandler(w http.ResponseWriter, r * http.Request) {
-    r.ParseForm()
-    t, err := template.ParseFiles("FT_88_4_4FT_88_4_5.html")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    index := r.Form["index"]
-    fmt.Println(index)
-    t.Execute(w, nil )
-    fmt.Println("End displayHandler")
-    return
+// exists returns whether the given file or directory exists or not
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
 
+func getAdvFilename(index string) (string, error) {
+	i, err := strconv.Atoi(index)
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+		return " ", err
+	}
+	fmt.Println(index)
+
+	findex := i % ADV_LEN
+	filename := "../static/" + strconv.Itoa(findex) + ".html"
+
+	ret, _ := exists(filename)
+	if ret != true {
+		fmt.Println(filename + "not exist in the file")
+	}
+	return filename, nil
+
+}
+
+func displayHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	index := r.Form["index"]
+
+	filename, err := getAdvFilename(index[0])
+	if err != nil {
+		fmt.Println("displayHandler getAdvFilename failed")
+		filename = "0.html"
+	}
+	t, err := template.ParseFiles(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t.Execute(w, nil)
+	fmt.Println("End displayHandler")
+	return
+}
 
 func main() {
-   http.HandleFunc("/", rootHandler)
-   http.HandleFunc("/display" , displayHandler)
-  
-   err := http.ListenAndServe(":8080", nil)
-   if err != nil {
-      log.Fatal("God Like listen wrong: ", err.Error())
-   }
-   
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/display", displayHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("God Like listen wrong: ", err.Error())
+	}
+
 }
